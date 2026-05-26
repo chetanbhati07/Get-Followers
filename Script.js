0144948f89bf68cf00db773dfd166dcd747499ac
Chetan Bhati <naichetand@gmail.com>
2026-05-26 17:23:46 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -18,3 +18,36 @@ let timer = setInterval(function(){
     }
 
 }, 1000);
+
+// 10 minutes ko seconds me convert kiya (10 * 60 = 600 seconds)
+let startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const timerEl = document.getElementById('timer2');
+
+// Har 1 second (1000ms) baad timer ko update karne ke liye interval lagaya
+const countdown = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    // Minutes aur Seconds calculate karein
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    // Agar single digit ho (jaise 9, 8, 7...) toh aage '0' add karein (09, 08...)
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    // HTML ke andar timer display karna
+    timerEl.innerHTML = `${minutes}:${seconds}`;
+
+    // Jab timer 0 ho jaye toh rok do
+    if (time <= 0) {
+        clearInterval(countdown);
+        timerEl.innerHTML = "00:00"; // Timer khatam hone par display
+    } else {
+        time--; // Ek-ek second kam karte raho
+    }
+}
+
+// Pehli baar bina delay ke turant timer show karne ke liye call kiya
+updateCountdown();
